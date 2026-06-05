e9192e48f54c5620b2950a12646454b1b78f0263
Nandhinisureshs <nandhinisuresh1006@gmail.com>
2026-06-05 16:32:09 +0530

    Add List_Vector class demonstrating Vector usage and capacity management

---
+++ Collection/List_Vector.java
@@ -0,0 +1,21 @@
+package Collection;
+
+import java.util.Vector;
+
+public class List_Vector {
+    public static void main(String[] args) {
+        Vector<Integer> v = new Vector<>(2);
+        System.out.println("Initial Capacity: "+ v.capacity());
+        v.add(25);
+        v.add(65);
+        System.out.println("After adding 2 capacity: "+v.capacity());
+        v.add(45);
+        v.add(89);
+        v.add(23);
+        v.add(77);
+        System.out.println("Final Capacity: "+v.capacity());
+
+        
+    }
+
+}
