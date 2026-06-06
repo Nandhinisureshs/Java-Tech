418e87dcf07e247abebda834338862b3fcc0f0d3
Nandhinisureshs <nandhinisuresh1006@gmail.com>
2026-06-06 12:02:16 +0530

    Add LinkedHS class to demonstrate usage of LinkedHashSet and TreeSet

---
+++ Collections/LinkedHS.java
@@ -0,0 +1,28 @@
+import java.util.LinkedHashSet;
+import java.util.TreeSet;
+public class LinkedHS {
+    public static void main(String[] args) {
+        
+        LinkedHashSet<String> movies = new LinkedHashSet<>();
+        //yentha order la insert pannamo antha order tha print agum atha LHS(linkedhashset)
+        movies.add("96");
+        movies.add("Leo");
+        movies.add("veeram");
+        movies.add("Karuppu");
+        movies.add("with love");
+
+        System.out.println(movies);
+
+        TreeSet<String> games = new TreeSet();
+        //sorted order
+        games.add("Free fire");
+        games.add("God of war");
+        games.add("Cricket");
+        games.add("Carrom");
+
+        System.out.println(games);
+
+    
+    }
+    
+}
